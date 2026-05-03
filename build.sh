a74b400df60456d783cd8471a8c7a900f385bfc0
sanjuthareddy <sanjuthareddy17@gmail.com>
2026-05-03 23:50:47 +0530

    Fix build with maven install script

---
+++ build.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/bash
+curl -fsSL https://archive.apache.org/dist/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz | tar xz
+export PATH=$PWD/apache-maven-3.9.6/bin:$PATH
+mvn package -q
